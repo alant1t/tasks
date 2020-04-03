af837e162963525d3d6f3edbbf64719a1e6abcf9
alant1t <52963791+alant1t@users.noreply.github.com>
2020-04-03 16:49:21 +0300

    Diminished code to C++98

---
+++ FileHandler.cpp
@@ -0,0 +1,138 @@
+#include "FileHandler.h"
+
+FileHandler::FileHandler() {
+	m_fileName.clear();
+	m_fileOutputStrings.clear();
+	m_dataContainer = NULL;
+}
+
+FileHandler::~FileHandler() {
+
+}
+
+void FileHandler::initialize() {
+	m_fileName.clear();
+	m_fileOutputStrings.clear();
+	m_dataContainer = NULL;
+}
+
+//----------------------------------
+// Загрузка данных из файла.
+//----------------------------------
+void FileHandler::read(string fileName, Points* dataContainer) {
+	m_fileName = fileName;
+	m_dataContainer = dataContainer;
+	
+	m_fileOutputStrings.clear();
+	this->openFile(fileName);
+	this->analyzeFile();
+}
+
+void FileHandler::openFile(string fileName) {
+	fstream inf;
+	inf.open(m_fileName, ios::in);
+	if (!inf.is_open()) {
+		string error = "Err!!Не удалось открыть файл.";
+		throw error;
+	}
+
+	for (string str; getline(inf, str); ) {
+		m_fileOutputStrings.push_back(str);
+	}
+
+	if (inf.bad()) {
+		string error = "Err!!Ошибка при чтении файла.";
+		throw error;
+	}
+	else if (inf.eof()) {
+
+	}
+	else if (inf.fail()) {
+		string error = "Err!!Неверный формат файла.";
+		throw error;
+	}
+
+	inf.close();
+	return;
+}
+
+void FileHandler::analyzeFile() {
+	vector<string>::iterator it = m_fileOutputStrings.begin();
+	if (it == m_fileOutputStrings.end()) {
+		string error = "Err!!Файл пуст.";
+		throw error;
+	}
+	
+	// Data block analysis.
+	// string in file looks like this:
+	// [-0.59910108  0.75331446]
+	Points tempContainer;
+
+	string			buff;
+	vector<double>	coordinates;
+	char			c;
+	while (it != m_fileOutputStrings.end()) {
+		if (*it == "") {
+			continue;
+		}
+		
+		buff.clear();
+		coordinates.clear();
+;		for (int i = 1; i < (it->size() - 1); i++ ) {
+			c = it->at(i);
+			if ( c != ' ' ) {
+				buff.push_back(c);
+			}
+			else {
+				if (buff.empty()) {
+					continue;
+				}
+				coordinates.push_back(stod(buff));
+				buff.clear();
+			}			
+		}
+		if ( !buff.empty() ) {
+			coordinates.push_back(stod(buff));
+		}
+		tempContainer.push_back(Point(coordinates));
+		
+		it++;
+	}
+	*m_dataContainer = tempContainer;
+	return;
+}
+
+
+
+
+//----------------------------------
+// Проверка данных на полноту.
+//----------------------------------
+void FileHandler::checkData(Points* dataContainer) {
+	
+}
+
+
+
+//----------------------------------
+// Выгрузка текстового документа.
+//----------------------------------
+void FileHandler::write(string fileName, Points& dataContainer) {
+	fstream outf;
+	outf.open(fileName, ios::out);
+	if (!outf.is_open()) {
+		string error = "Err!!Не удалось открыть файл.";
+		throw error;
+	}
+
+	Points::iterator it = dataContainer.begin();
+	while (it != dataContainer.end()) {
+		outf << it->getCoordinate(0) << " "
+			<< it->getCoordinate(1) << " "
+			<< it->getState() << "\n";
+		it++;
+	}
+
+	outf.close();
+	return;
+}
---
+++ PointContainer.cpp
@@ -0,0 +1,95 @@
+#include "PointContainer.h"
+
+Point::Point() {
+	m_source = nullptr;
+	m_coordinates.clear();
+	m_state = -1; // not visited
+}
+
+Point::Point(vector<double>& coordinates) {
+	m_source = nullptr;
+	m_coordinates.clear();
+	m_coordinates = coordinates;
+	m_state = -1; // not visited
+}
+
+Point::~Point() {
+
+}
+
+void Point::clear() {
+	m_coordinates.clear();
+	m_state = -1; // not visited
+}
+
+int Point::getSize() {
+	return m_coordinates.size();
+}
+
+void Point::pushCoordinate(double* coordinate) {
+	if (coordinate != NULL) {
+		m_coordinates.push_back(*coordinate);
+	}
+}
+
+double	Point::getCoordinate(int no) {
+	return m_coordinates.at(no);
+}
+
+bool Point::isVisited() {
+	if (m_state == -1) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+int Point::getState() {
+	return m_state;
+}
+
+void Point::setState(int state) {
+	m_state = state;
+	return;
+}
+
+void Point::setSource(void* source) {
+	m_source = source;
+}
+
+void Point::getSource(void* source) {
+	source = m_source;
+}
+
+
+
+Cluster::Cluster()
+{
+}
+
+Cluster::~Cluster()
+{
+}
+
+void Cluster::setCoords(vector<double>& abscisses, vector<double>& ordinates)
+{
+	m_abscisses = abscisses;
+	m_ordinates = ordinates;
+}
+
+void Cluster::getCoords(vector<double>& abscisses, vector<double>& ordinates)
+{
+	abscisses = m_abscisses;
+	ordinates = m_ordinates;
+}
+
+void Cluster::pushX(double x)
+{
+	m_abscisses.push_back(x);
+}
+
+void Cluster::pushY(double y)
+{
+	m_ordinates.push_back(y);
+}
